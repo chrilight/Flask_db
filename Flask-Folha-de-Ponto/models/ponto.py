ca94c86c6c35d272327a4fee2ab8a36bae92b7d0
Christian Lucas <162521696+chrilight@users.noreply.github.com>
2024-10-31 19:44:48 -0300

    organização de diretórios

---
+++ Flask-Folha-de-Ponto/models/ponto.py
@@ -0,0 +1,13 @@
+# app/ponto.py
+from database.session import db
+
+class Ponto(db. model):
+    __tablename__ = 'Ponto'
+
+    id = db.Column(db.Integer, primary_key=True)
+    usuario = db.Column(db.String(50), nullable=False)
+    horario_entrada = db.Column(db.DateTime, nullable=True)
+    horario_saida = db.Column(db.DateTime, nullable=True)
+
+    def __repr__(self):
+        return f'<Ponto {self.usuario} - Entrada: {self.horario_entrada}, Saída: {self.horario_saida}>'
